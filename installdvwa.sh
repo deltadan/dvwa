79a3881c976a52dcfe938edf8ff1dab4d370813b
dan patrick <dan@clouddeltav.com>
2020-04-30 08:29:11 -0400

    update to build two dvwa instances

--- installdvwa.sh
+++ installdvwa.sh
@@ -1,13 +1,21 @@
 # Custom Script for Linux
 #!/bin/bash
-sudo wget https://github.com/deltadan/dvwa/raw/master/dvwa1.zip
-sudo wget https://github.com/deltadan/dvwa/raw/master/dvwa2.zip
+#set parameters for server installs
+sudo apt-get install -y apache2 mysql-server php php-mysqli php-gd libapache2-mod-php unzip
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password p@ssw0rd'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password p@ssw0rd'
-sudo apt-get install -y apache2 mysql-server php php-mysqli php-gd libapache2-mod-php unzip
 sudo rm /var/www/html/index.html
+#install first dvwa instance
+sudo wget https://github.com/deltadan/dvwa/raw/master/dvwa1.zip
+mkdir /var/www/html/dvwa1
+sudo mv dvwa1.zip /var/www/html/dvwa1
+sudo unzip /var/www/html/dvwa1/dvwa1.zip -d /var/www/html/dvwa1/
+sudo chmod -R 777 /var/www/html/dvwa1/hackable/uploads/
+#install second first dvwa instance
+sudo wget https://github.com/deltadan/dvwa/raw/master/dvwa2.zip
 mkdir /var/www/html/dvwa2
 sudo mv dvwa2.zip /var/www/html/dvwa2
 sudo unzip /var/www/html/dvwa2/dvwa2.zip -d /var/www/html/dvwa2/
 sudo chmod -R 777 /var/www/html/dvwa2/hackable/uploads/
+# restart apache after installs
 sudo apache2ctl restart
