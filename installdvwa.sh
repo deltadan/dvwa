430d96a54d190d2c39eaf2e9d37c4b6d561ad019
dan patrick <dan@clouddeltav.com>
2020-04-30 08:15:01 -0400

    update dir in zip file

--- installdvwa.sh
+++ installdvwa.sh
@@ -7,7 +7,7 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again p
 sudo apt-get install -y apache2 mysql-server php php-mysqli php-gd libapache2-mod-php unzip
 sudo rm /var/www/html/index.html
 mkdir /var/www/html/dvwa2
-sudo mv dvwa1.zip /var/www/html/dvwa2
-sudo unzip /var/www/html/dvwa2/dvwa1.zip -d /var/www/html/dvwa2/
+sudo mv dvwa2.zip /var/www/html/dvwa2
+sudo unzip /var/www/html/dvwa2/dvwa2.zip -d /var/www/html/dvwa2/
 sudo chmod -R 777 /var/www/html/dvwa2/hackable/uploads/
 sudo apache2ctl restart
